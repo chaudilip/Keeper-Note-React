667f50ca611625ef1f2b62eaada64cd66d1679a9
chaudilip <dilipchau3602@gmail.com>
2023-02-01 21:20:58 +0530

    second commit

---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>This is title for keeper App</h1>
+            <p>This is Normal And Super random paragraph for Content</p>
+        </div>
+    )
+}
+
+export default Note;
